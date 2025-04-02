264bf6c4d3138e2cadeffe99dbac6c3dba71049b
Kümmel Christian <c.kuemmel@microstream.one>
2025-04-02 11:37:56 +0200

    Upgrade Micronaut to 8.0.0 and EclipseStore to 2.1.3

--- src/main/java/one/microstream/domain/RAuthor.java
+++ src/main/java/one/microstream/domain/RAuthor.java
@@ -1,5 +1,8 @@
 package one.microstream.domain;
 
+import io.micronaut.serde.annotation.Serdeable;
+
+@Serdeable
 public record RAuthor(String id, String	firstname, String lastname, String email, String gender, String alias)
 {
 	
